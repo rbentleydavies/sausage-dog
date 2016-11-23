090fdc0089c0b9aff5b032b553488c51d9ef0d5a
Ross Bentley-Davies <ross.b@mmtdigital.co.uk>
2016-11-23 12:01:08 +0000

    Added SMTP server

--- buildfiles/sausage.js
+++ buildfiles/sausage.js
@@ -1,1 +1,12 @@
-console.log('sausages');
+const {SMTPServer} = require('smtp-server');
+
+const server = new SMTPServer({
+    logger: true,
+    onData(stream, session, callback){
+        stream.pipe(process.stdout); // print message to console
+        stream.on('end', callback);
+    },
+    disabledCommands: ['AUTH', 'STARTTLS'],
+});
+
+server.listen(25);
