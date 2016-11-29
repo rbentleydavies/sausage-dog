8abc4a8ccc378e6805d66db2d97daa6f31d15938
rbentleydavies <rossbd@rossbeniessimac.lan>
2016-11-29 19:28:32 +0000

    Web server on web port for production

--- src/sausage.js
+++ src/sausage.js
@@ -3,5 +3,5 @@ import startwebserver from './startwebserver.js'
 import startpopserver from './startpopserver.js'
 
 startmailserver();
-startwebserver(3000);
+startwebserver(80);
 startpopserver();
